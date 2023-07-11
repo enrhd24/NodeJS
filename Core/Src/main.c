6bc38ab3d0e131ea4656fb00bd2aca1548b3070b
enrhd24 <enrhd567@gmail.com>
2023-07-11 09:09:13 +0900

    nucleo -f103rb start uart printf

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -63,46 +63,26 @@ static void MX_USART1_UART_Init(void);
   * @brief  The application entry point.
   * @retval int
   */
+
+void SystemClock_Config(void);
+UART_HandleTypeDef hlpuart1;
+
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
   SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART1_UART_Init();
-  /* USER CODE BEGIN 2 */
 
-  /* USER CODE END 2 */
+  const char * hello_world = "Hello world\r\n";
+  HAL_UART_Transmit(&hlpuart1, (uint8_t *)hello_world, strlen(hello_world), HAL_MAX_DELAY);
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
   while (1)
   {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
-
+	  printf("Hello %s\r\n", "world");
   }
-  /* USER CODE END 3 */
 }
+
 
 /**
   * @brief System Clock Configuration
